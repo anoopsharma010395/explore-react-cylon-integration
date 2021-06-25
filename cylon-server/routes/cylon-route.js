a21c3dbaba30cf117fc05f52b61df74cce5fbff6
Anoop Sharma <38686430+anoopsharma010395@users.noreply.github.com>
2021-06-25 11:13:21 +0530

    Update cylon-route.js

--- cylon-server/routes/cylon-route.js
+++ cylon-server/routes/cylon-route.js
@@ -1,32 +1,19 @@
 const express = require('express');
-const Cylon = require('cylon');
 const router = express.Router();
-const LEDRobot = require('../robots/led-robot');
 const OpenRobot = require('../robots/open-robot');
 
-router.post('/setLedRobotConfiguration', (req, res) => {
-  // Initialize the LED Robot
-  console.log('req is :: ', req.body);
-  console.log('Type', typeof req.body);
-  LEDRobot.setLedRobotConfiguration(req.body);
-  res.send('LED Configuration Successful!');
-});
-router.get('/startLED', function (req, res, next) {
-  // Start the LED
-  LEDRobot.startLED();
-  res.send('LED Started');
-});
-router.get('/stopLED', function (req, res, next) {
-  // Start the LED
-  LEDRobot.stopLED();
-  res.send('LED Stopped');
+router.get('/startWebCam', (req, res) => {
+  // Initialize and start webcam
+  OpenRobot.startWebCam();
+  res.send('Camera Started!');
 });
 
-router.get('/setopencvRobotConfiguration', (req, res) => {
-  // Initialize and start webcam
-  console.log('setopencvRobotConfiguration');
-  OpenRobot.setOpenCvRobotConfiguration();
-  res.send('setopencvRobotConfiguration!');
+router.get('/stopWebCam', (req, res) => {
+  // Stop webcam
+  OpenRobot.stopWebCam();
+  res.send('Camera Stopped!');
 });
+
+
 
 module.exports = router;
